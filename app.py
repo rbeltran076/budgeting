322f188f91fbd42d8f15136e7461e840a174955c
Ronald <rbeltran076@gmail.com>
2024-05-02 16:36:52 +0200

    file input and tabulator finally work!!

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ import numpy as np
 import io
 # from matplotlib.figure import Figure
 
-ACCENT = "goldenrod"
-LOGO = "https://assets.holoviz.org/panel/tutorials/matplotlib-logo.png"
-
-pn.extension(design='material', sizing_mode="stretch_width")
+pn.extension('tabulator', design='material', sizing_mode="stretch_width")
 
 
 # CONTENT =======================================================================================
@@ -51,25 +48,24 @@ introText = pn.pane.Markdown(markdown_text)
 # Create a file input widget
 file_input = pn.widgets.FileInput(accept='.csv')
 
-# create a dataframe widget 
-dataframe = pn.widgets.DataFrame(
-    pd.DataFrame({
-        'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-        'Age': [25, 30, 35, 40, 45],
-        'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Miami']
-    }),
-    name='this is the name of the dataframe'
-)
+# create a tabulator widget 
+table = pn.widgets.Tabulator(pagination='remote', page_size=10)
 
 # function to display a csv head when one is uploaded to file_input
 # DOESNT WORK YET
 def display_csv_head(event): 
-    if isinstance(event.new, io.BytesIO):  # Check if we have the actual file data
-        df = pd.read_csv(event.new)
-        dataframe.value = df.head()
+    print(event.new)
+    # Check if we have the actual file data
+    if event.new:             
+        print("\n\n\nNEW EVENT DETECTED\n\n")              
+        # read the csv as 
+        df = pd.read_csv(io.BytesIO(event.new))
+        print(df)
+        # Update the data attribute of dataframe
+        table.value = df.
 
-# binding the file_input widget to the function
-# file_input.param.watch(display_csv_head, 'value')
+# Bind the file_selector widget to the function
+file_input.param.watch(display_csv_head, 'value')
 
 
 # LAYOUT DISTRIBUTION OF ALL ELEMENTS =========================================================
@@ -77,6 +73,6 @@ pn.Row(
     introText,
     pn.Column(
         file_input,
-        dataframe
+        table
     )
 ).servable()
