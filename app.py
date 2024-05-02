8858985c9493420506b5c06974ec91dca6fa0f6e
Ronald <rbeltran076@gmail.com>
2024-05-02 18:38:19 +0200

    Implemented Tabs. Working of Info display

--- app.py
+++ app.py
@@ -9,70 +9,60 @@ pn.extension('tabulator', design='material', sizing_mode="stretch_width")
 
 # CONTENT =======================================================================================
 
-markdown_text = """
-# Sample Markdown Text
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed **sagittis** lacus nec **diam** facilisis, id *dapibus* justo *ultricies*. Fusce vel **odio** nec mauris eleifend vehicula. 
-
-## Subheading
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis lacus nec diam facilisis, id dapibus justo ultricies. Fusce vel odio nec mauris eleifend vehicula. 
-
-### Sub-subheading
-
-![Sample Image](https://via.placeholder.com/150)
-
-- Lorem ipsum dolor sit amet
-- Consectetur adipiscing elit
-- Sed sagittis lacus nec diam facilisis
-- Fusce vel odio nec mauris eleifend vehicula
-
-1. Lorem ipsum dolor sit amet
-2. Consectetur adipiscing elit
-3. Sed sagittis lacus nec diam facilisis
-4. Fusce vel odio nec mauris eleifend vehicula
-
-#### Sub-sub-subheading
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis lacus nec diam facilisis, id dapibus justo ultricies. Fusce vel odio nec mauris eleifend vehicula. 
-
-> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis lacus nec diam facilisis, id dapibus justo ultricies. Fusce vel odio nec mauris eleifend vehicula.
-
-*Lorem ipsum* dolor sit amet, consectetur adipiscing elit. Sed sagittis lacus nec diam facilisis, id dapibus justo ultricies. Fusce vel odio nec mauris eleifend vehicula. 
-
-**Lorem ipsum** dolor sit amet, consectetur adipiscing elit. Sed sagittis lacus nec diam facilisis, id dapibus justo ultricies. Fusce vel odio nec m
-"""
-
-introText = pn.pane.Markdown(markdown_text)
+# read the introText
+with open('text/intro.txt', 'r') as file:
+    introText = pn.pane.Markdown(file.read())
 
 # Create a file input widget
 file_input = pn.widgets.FileInput(accept='.csv')
 
 # create a tabulator widget 
-table = pn.widgets.Tabulator(pagination='remote', page_size=10)
+table = pn.widgets.Tabulator(pagination='remote', page_size=20, width=700, sizing_mode='stretch_height')
+pn.widgets.Tabulator.theme = 'semantic-ui'
+
+# create a Str pane
+dataInfo = pn.pane.Str()
+dataInfo.object = """Hello
+This is me
+"""
+
+
+# FUNCTIONS FOR DYNAMIC APP FUNCTIONALITIES =========================================================
 
 # function to display a csv head when one is uploaded to file_input
-# DOESNT WORK YET
 def display_csv_head(event): 
-    print(event.new)
+    print(event.new)  # fpd
     # Check if we have the actual file data
-    if event.new:             
-        print("\n\n\nNEW EVENT DETECTED\n\n")              
+    if event.new:   
+        print("\n\n\nNEW EVENT DETECTED\n\n") # fpd              
         # read the csv as 
         df = pd.read_csv(io.BytesIO(event.new))
-        print(df)
+        # print(df)  # fpd
         # Update the data attribute of dataframe
-        table.value = df.
+        table.value = df
 
 # Bind the file_selector widget to the function
 file_input.param.watch(display_csv_head, 'value')
 
+def display_csv_info(event):
+    if event.new:
+        df = pd.read_csv(io.BytesIO(event.new))
+        info = str(df.info())
+        print(info)
+        dataInfo.object = f"""{df.info()}"""
+
+# Bind the file_selector widget to the function
+file_input.param.watch(display_csv_info, 'value')
+
 
 # LAYOUT DISTRIBUTION OF ALL ELEMENTS =========================================================
 pn.Row(
-    introText,
     pn.Column(
-        file_input,
-        table
+        introText,
+        file_input
+    ),
+    pn.Tabs(
+        ("Table", table),
+        ("Info", dataInfo),
     )
 ).servable()
